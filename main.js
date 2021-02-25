a8b0bf44e8ab926c1de1cd9c5fcf1334133d4250
hknktk <75674444+hknktk@users.noreply.github.com>
2021-02-25 12:14:06 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -26,13 +26,25 @@ document.addEventListener('DOMContentLoaded',function(){
   })
 
 
-    const targetElement = document.querySelectorAll(".js-target");
+    const targetElement = document.querySelectorAll(".js-text");
     document.addEventListener("scroll", function(){
     for (let i = 0; i<targetElement.length; i++){
     const getElementDistance =targetElement[i].
-    getBoundingClientRect().top + targetElement[i].clientHeight * .8
+    getBoundingClientRect().top + targetElement[i].clientHeight * .9
        if(window.innerHeight > getElementDistance){
        targetElement[i].classList.add("show");
+        }
+      }
+    })
+
+
+    const targetElement2 = document.querySelectorAll(".js-sub");
+    document.addEventListener("scroll", function(){
+    for (let i = 0; i<targetElement2.length; i++){
+    const getElementDistance =targetElement2[i].
+    getBoundingClientRect().top + targetElement2[i].clientHeight * .9
+       if(window.innerHeight > getElementDistance){
+       targetElement2[i].classList.add("show");
         }
       }
     })
